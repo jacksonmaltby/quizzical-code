26c3abbfb1ec08bed1556d28c4e2d201b7448a23
J Maltby <jacksonmaltby@gmail.com>
2023-01-24 21:33:44 -0800

    Edited JS to fix answer buttons, added name entering function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,10 @@ const quiz = [
     },
 ];
 
-let timeLeft = 120;
+let timeLeft = 90;
 let score = 0;
 let currentQuestion = 0;
+let intervalId;
 
 const startQuiz = document.getElementById("start-button");
 const form = document.getElementById("quiz-form");
@@ -32,33 +33,43 @@ function start() {
     intro.style.display = "none";
     startQuiz.style.display = "none";
     form.style.display = "block";
+    form.addEventListener("click", e => {
+        if (e.target.classList.contains("option")) {
+            checkAnswer(e);
+        }
+    });
     showQuestion();
     countdown();
 }
 
 function showQuestion() {
+    form.innerHTML = "";
     if (currentQuestion === quiz.length) {
         showResults();
         return;
     }
     const question = quiz[currentQuestion];
-    form.innerHTML = `
-      <h2>${question.question}</h2>
-      ${question.options.map((option, index) => `<button class="option" name="answer" value="${option}">${option}</button>`).join("")}
-      <br>
-      <div id="message"></div>
-      <br>
-      <span id="timer"></span>
-    `;
+    const questionEl = document.createElement("h2");
+    questionEl.innerHTML = question.question;
+    form.appendChild(questionEl);
+    question.options.forEach(option => {
+        const optionEl = document.createElement("button");
+        optionEl.classList.add("option");
+        optionEl.name = "answer";
+        optionEl.value = option;
+        optionEl.innerHTML = option;
+        optionEl.addEventListener("click", checkAnswer);
+        form.appendChild(optionEl);
+    });
 }
 
 function countdown() {
     const timer = document.getElementById("timer");
-    const interval = setInterval(() => {
+    intervalId = setInterval(() => {
         timeLeft--;
         timer.innerHTML = `Time left: ${timeLeft} seconds`;
         if (timeLeft === 0) {
-            clearInterval(interval);
+            clearInterval(intervalId);
             showResults();
         }
     }, 1000);
@@ -66,7 +77,15 @@ function countdown() {
 
 function checkAnswer(e) {
     e.preventDefault();
-    const selectedOption = document.querySelector('.option:checked');
+    const options = document.querySelectorAll(".option");
+    options.forEach(option => {
+        if (option === e.target) {
+            option.setAttribute("checked", true);
+        } else {
+            option.removeAttribute("checked");
+        }
+    });
+    const selectedOption = document.querySelector('.option[checked]');
     const message = document.getElementById("message");
     if (selectedOption.value === quiz[currentQuestion].answer) {
         score += 10;
@@ -85,8 +104,46 @@ function showResults() {
     form.style.display = "none";
     results.style.display = "block";
     results.innerHTML = `
-      <h2>Quiz Results</h2>
-      <p>Score: ${score}</p>
-      <p>Time Left: ${timeLeft} seconds</p>
+        <h2>Quiz Results</h2>
+        <p>Score: ${score}</p>
+        <p>Time Left: ${timeLeft} seconds</p>
     `;
+    let name = prompt("Enter your name:");
+    localStorage.setItem(name, score);
+    clearInterval(intervalId);
+    showHighScores();
+}
+
+function saveHighScore() {
+    let name = prompt("Enter your name:");
+    localStorage.setItem(name, score);
+}
+
+function displayHighScores() {
+    let highScores = document.getElementById("high-scores");
+    highScores.innerHTML = "";
+    let scores = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        scores.push({ name: key, score: value });
+    }
+
+    scores.sort((a, b) => {
+        return b.score - a.score;
+    });
+
+    for (let i = 0; i < 3; i++) {
+        let li = document.createElement("li");
+        li.innerHTML = `${scores[i].name} - ${scores[i].score}`;
+        highScores.appendChild(li);
+    }
+}
+
+let highScores = document.getElementById("high-scores");
+for (let i = 0; i < 3; i++) {
+    let score = scores[i];
+    let p = document.createElement("p");
+    p.innerHTML = `${score.name} : ${score.value}`;
+    highScores.appendChild(p);
 }
